97c59e72911df91ad675d64a7a9d32264c6c4db9
xiaoji-diary <1624475081@qq.com>
2025-10-07 00:12:49 +0800

    fix: 暂且更改生产环境的ip

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_BASE_URL = 'http://localhost:8080/api'
+const API_BASE_URL = 'http://47.96.66.53:8080/api'
 
 export const authApi = {
   login: (credentials) => axios.post(`${API_BASE_URL}/auth/login`, credentials),
